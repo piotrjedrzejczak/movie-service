820ff92bbf16423ac853cd5083d570bafa6cab79
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2021-04-09 22:46:55 +0200

    new ut

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
-
+API_KEY = "f8501a2c"
 STORAGE_FILENAME = "movies.json"
 STORAGE_FILEPATH = os.path.join(os.getcwd(), STORAGE_FILENAME)
+REQUIRED_FIELDS = ['imdbID', 'title', 'imdbRating', 'BoxOffice']
--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 import unittest
 import subprocess
 import os
-from config import STORAGE_FILEPATH
-from ms import parse_args
+from config import STORAGE_FILEPATH, REQUIRED_FIELDS
+from ms import parse_args, get_movie_details
 
 
 class MovieServiceBaseTest:
@@ -15,7 +15,7 @@ class MovieServiceBaseTest:
 
 
 class MovieServiceFunctionalTests(unittest.TestCase, MovieServiceBaseTest):
-    
+
     def test_download_movie_data_and_save_to_json_file(self):
         args = ["the matrix", "lord of the rings"]
         self.assertEqual(self.run_script(args).returncode, 0)
@@ -27,6 +27,21 @@ class MovieServiceUnitTests(unittest.TestCase, MovieServiceBaseTest):
     def test_argument_parser(self):
         args = ["the matrix", "lord of the rings"]
         self.assertEqual(args, parse_args(args).list)
+
+    def test_get_movie_details(self):
+        """This test might be prone to failures due to data changes
+        on the website. In case it fails make sure that the
+        expected_output is up to date."""
+
+        title = "the matrix"
+        expected_output = {
+            'imdbID': 'tt0133093',
+            'Title': 'The Matrix',
+            'imdbRating': '8.7',
+            'BoxOffice': '$171,479,930'
+        }
+        rv = get_movie_details(title, REQUIRED_FIELDS)
+        self.assertEqual(expected_output, rv)
 
 
 if __name__ == "__main__":
