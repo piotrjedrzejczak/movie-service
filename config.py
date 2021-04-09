5784c6bc63022061bdf46871d2b0e6d15f5f77ca
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2021-04-09 19:54:36 +0200

    moved config variables to separate file

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+
+STORAGE_FILENAME = "movies.json"
+STORAGE_FILEPATH = os.path.join(os.getcwd(), STORAGE_FILENAME)
--- tests.py
+++ tests.py
@@ -1,17 +1,13 @@
 import unittest
 import subprocess
-import json
 import os
-from ms import STORAGE_FILEPATH
+from config import STORAGE_FILEPATH
 
 
 class MovieServiceTest(unittest.TestCase):
     
     def test_download_movie_data_and_save_to_json_file(self):
-        rv = subprocess.run(
-            'python ms.py "the matrix", "lord of the rings"',
-            capture_output=True
-        )
+        rv = subprocess.run('python ms.py "the matrix" "lord of the rings"', capture_output=True)
         self.assertEqual(rv.returncode, 0)
         self.assertTrue(os.path.isfile(STORAGE_FILEPATH))
 
