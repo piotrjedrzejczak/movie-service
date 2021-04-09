36a1243dbafaecb60d2f303132250be723a937b6
Piotr Jedrzejczak <pjedrzejczak@sigma.ug.edu.pl>
2021-04-09 19:47:05 +0200

    first FT

---
+++ tests.py
@@ -0,0 +1,19 @@
+import unittest
+import subprocess
+import json
+import os
+from ms import STORAGE_FILEPATH
+
+
+class MovieServiceTest(unittest.TestCase):
+    
+    def test_download_movie_data_and_save_to_json_file(self):
+        rv = subprocess.run(
+            'python ms.py "the matrix", "lord of the rings"',
+            capture_output=True
+        )
+        self.assertEqual(rv.returncode, 0)
+        self.assertTrue(os.path.isfile(STORAGE_FILEPATH))
+
+if __name__ == "__main__":
+    unittest.main()
